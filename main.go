cc207432ea2fceb29c32c3f6a339e8d838d238cf
Obelixor-Team <Obelixor-Team@proton.me>
2025-12-03 10:24:45 +0000

    feat: Integrate tcell/v2 for terminal interaction, basic setup

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 func main() {
-	fmt.Println("Glitch Saver: Hello, Go!")
+	// Initialize tcell screen
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err = s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	// Set default style and clear screen
+	s.SetStyle(tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite))
+	s.Clear()
+
+	// Hide cursor
+	s.HideCursor()
+
+	// Event loop for handling input and drawing
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+	defer quit() // Ensure screen is finalized on exit
+
+	// Simple text to display
+	str := "Glitch Saver: Press ESC or 'q' to quit"
+	for i, r := range str {
+		s.SetContent(i, 0, r, nil, tcell.StyleDefault.Foreground(tcell.ColorGreen))
+	}
+	s.Show()
+
+	// Main event loop
+	for {
+		ev := s.PollEvent()
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Rune() == 'q' {
+				return
+			}
+		}
+		// Add a small delay to prevent busy-waiting for events, though tcell.PollEvent() is blocking
+		time.Sleep(time.Millisecond * 50)
+	}
 }
